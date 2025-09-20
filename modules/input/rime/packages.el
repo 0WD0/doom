9c5d9af6ce09be2cf5c2705b04d69117a060fa23
WD <wd.1105848296@gmail.com>
2025-09-20 11:10:49 +0800

    use 0WD0/ace-pinyin

--- modules/input/rime/packages.el
+++ modules/input/rime/packages.el
@@ -5,7 +5,8 @@
 (package! pangu-spacing :pin "6509df9c90bbdb9321a756f7ea15bb2b60ed2530")
 (when (modulep! +childframe)
   (package! posframe :pin "12f540c9ad5da09673b2bca1132b41f94c134e82"))
-(package! ace-pinyin :pin "47662c0b05775ba353464b44c0f1a037c85e746e")
+(package! ace-pinyin
+  :recipe (:local-repo "ace-pinyin"))
 (when (modulep! :editor evil +everywhere)
   (package! evil-pinyin
     :recipe (:build (:not autoloads))
