d99461ce0faf21f282c30b2692f4dce20074815a
0WD0 <1105848296@qq.com>
2025-11-02 19:05:00 +0800

    khalel

--- init.el
+++ init.el
@@ -162,7 +162,7 @@
        lua                                                                                ; one-based indices? one-based indices
        (markdown +grip)                                                                   ; writing docs for people to ignore
        ;;nim                                                                              ; python + lisp at the speed of c
-       ;;nix                                                                              ; I hereby declare "nix geht mehr!"
+       (nix +lsp +tree-sitter)                                                            ; I hereby declare "nix geht mehr!"
        ;;ocaml                                                                            ; an objective camel
        (org +roam +dragndrop +hugo +noter +pandoc +pomodoro +present +jupyter +passwords) ; organize your plain life in plain text
        ;;php                                                                              ; perl's insecure younger brother
