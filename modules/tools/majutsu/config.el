bf028db4e8d3879b3b62f2da7d531206ce0f6204
0WD0 <1105848296@qq.com>
2025-11-19 10:20:13 +0800

    update

--- modules/tools/majutsu/config.el
+++ modules/tools/majutsu/config.el
@@ -5,8 +5,8 @@
   :commands (majutsu majutsu-log
                      majutsu-rebase-transient majutsu-bookmark-transient majutsu-git-transient
                      majutsu-commit majutsu-describe majutsu-diff majutsu-diffedit-emacs majutsu-diffedit-smerge
-                     majutsu-log-refresh majutsu-mode-transient majutsu-squash-transient
-                     majutsu-abandon majutsu-undo majutsu-new majutsu-enter-dwim majutsu-goto-current))
+                     majutsu-log-refresh majutsu-dispatch majutsu-squash-transient
+                     majutsu-abandon majutsu-undo majutsu-new majutsu-enter-dwim majutsu-log-goto-@))
 
 ;; Keybindings: `SPC j` prefix (evil leader)
 (when (modulep! :editor evil)
@@ -19,48 +19,3 @@
          :desc "Commit"            "c" #'majutsu-commit
          :desc "Describe"          "d" #'majutsu-describe
          :desc "Diff"              "D" #'majutsu-diff)))
-
-;; Per-mode evil bindings (override upstream in a safe, lazy way)
-(after! majutsu
-  (when (modulep! :editor evil)
-    (add-hook 'majutsu-mode-hook #'turn-off-evil-snipe-mode)
-    (add-hook 'majutsu-mode-hook #'turn-off-evil-snipe-override-mode)
-    (map! :map majutsu-mode-map
-          :nv "." #'majutsu-goto-current
-
-          :nv "R" #'majutsu-log-refresh
-          :nv "gr" #'majutsu-log-refresh
-          :nv "c" #'majutsu-commit
-          :nv "e" #'majutsu-edit-changeset
-          :nv "u" #'majutsu-undo
-          :nv "C-r" #'majutsu-redo
-          ;; :nv "N" #'majutsu-new
-          :nv "s" #'majutsu-squash-transient
-          :nv "l" #'majutsu-log-transient
-          :nv "d" #'majutsu-describe
-          :nv "x" #'majutsu-abandon
-
-          :nv "b" #'majutsu-bookmark-transient
-          :nv "r" #'majutsu-rebase-transient
-          ;; :nv "G" #'majutsu-git-transient
-
-          :nv "D" #'majutsu-diff
-          :nv "E" #'majutsu-diffedit-emacs
-          :nv "M" #'majutsu-diffedit-smerge
-          :nv "?" #'majutsu-mode-transient
-
-          :nv "]" #'magit-section-forward-sibling
-          :nv "[" #'magit-section-backward-sibling
-          ;; Make RET consistent even if upstream changes default
-          ;; :n  [remap magit-visit-thing] #'majutsu-enter-dwim
-          :n  "RET" #'majutsu-enter-dwim
-	  )
-    (map! :map majutsu-diff-mode-map
-          :nv "RET" #'majutsu-enter-dwim
-          )
-    (map! :localleader
-          :map majutsu-mode-map
-          :nv "n" #'majutsu-new
-          :nv "g" #'majutsu-git-transient
-          )
-    ))
